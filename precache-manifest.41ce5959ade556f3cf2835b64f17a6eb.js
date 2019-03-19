76be9bf21df9421cc3ea3955ee36ae27b97f80cf
Vaibhav Mohan P <corra-230@CORRA-230.local>
2019-03-20 00:42:56 +0530

    Updates

---
+++ precache-manifest.41ce5959ade556f3cf2835b64f17a6eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "dda2c0292837330869af",
+    "url": "/contactmanager/static/js/main.7e19fc5b.chunk.js"
+  },
+  {
+    "revision": "ab08168798d3ada6a224",
+    "url": "/contactmanager/static/js/2.20d82ee2.chunk.js"
+  },
+  {
+    "revision": "dda2c0292837330869af",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "ab08168798d3ada6a224",
+    "url": "/contactmanager/static/css/2.27611094.chunk.css"
+  },
+  {
+    "revision": "c74dc69cf4eddab0821d7653464e6ded",
+    "url": "/contactmanager/index.html"
+  }
+];
